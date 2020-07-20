76daa94888b3c3b200b46b9fde580b69e2386db4
“Brandon <Brandon.joseph.martinez3@gmail.com>
2020-07-20 14:15:27 -0500

    Create tables_exercises with content

---
+++ database-exercises/tables_exercises.sql
@@ -0,0 +1,10 @@
+
+
+USE employees;
+SHOW TABLES;
+DESCRIBE employees; #int, date, varchar, enum
+#salaries
+#titles, departments
+#dept_manager
+DESCRIBE departments; # there are varchar types?
+SHOW CREATE TABLE dept_manager; 
