5155df129b42c77defc8be8afb89058d5519e131
“Brandon <Brandon.joseph.martinez3@gmail.com>
2020-07-20 14:22:03 -0500

    updated responses

--- database-exercises/tables_exercises.sql
+++ database-exercises/tables_exercises.sql
@@ -6,5 +6,5 @@ DESCRIBE employees; #int, date, varchar, enum
 #salaries
 #titles, departments
 #dept_manager
-DESCRIBE departments; # there are varchar types?
+DESCRIBE departments; # no direct relationship except in a separate table that combines both
 SHOW CREATE TABLE dept_manager; 
