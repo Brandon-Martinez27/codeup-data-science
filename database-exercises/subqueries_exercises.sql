db2714f33016a1532a8a381f5ed68979481eccae
“Brandon <Brandon.joseph.martinez3@gmail.com>
2020-07-26 12:10:33 -0500

    Finished Subqueries Exercises

--- database-exercises/subqueries_exercises.sql
+++ database-exercises/subqueries_exercises.sql
@@ -1,17 +1,17 @@
-# 1. Find all the employees with the same hire date as employee 101010 using a sub-query
-# 69 Rows
+-- 1. Find all the employees with the same hire date as employee 101010 using a sub-query
+-- 69 Rows
 
 USE employees;
-SELECT first_name, last_name #display the employee names columns
-FROM employees				 #from the employees table
-WHERE hire_date IN ( 		 #filter results to when the hire date is in
-	SELECT hire_date		 #the hire date
-	FROM employees			 #from employees table
-	WHERE emp_no = 101010	 #that is the same as employee 101010
+SELECT first_name, last_name --display the employee names columns
+FROM employees				 --from the employees table
+WHERE hire_date IN ( 		 --filter results to when the hire date is in
+	SELECT hire_date		 --the hire date
+	FROM employees			 --from employees table
+	WHERE emp_no = 101010	 --that is the same as employee 101010
 );
 
-# 2. Find all the titles held by all employees with the first name Aamod.
-# 314 total titles
+-- 2. Find all the titles held by all employees with the first name Aamod.
+-- 314 total titles
 SELECT title
 FROM titles
 WHERE emp_no IN (
@@ -20,7 +20,7 @@ WHERE emp_no IN (
 	WHERE first_name = 'Aamod'
 );
 
-# '6' unique titles
+-- '6' unique titles
 SELECT title
 FROM titles
 WHERE emp_no IN (
@@ -31,7 +31,7 @@ WHERE emp_no IN (
 GROUP BY title;
 
 
-# 3. How many people in the employees table are no longer working for the company?
+-- 3. How many people in the employees table are no longer working for the company?
 
 SELECT COUNT(DISTINCT emp_no) as no_longer_working
 FROM dept_emp
@@ -41,7 +41,7 @@ WHERE emp_no IN (
 )
 AND to_date <= CURDATE();
 
-# 4. Find all the current department managers that are female.
+-- 4. Find all the current department managers that are female.
 
 SELECT first_name, last_name
 FROM employees
@@ -52,8 +52,8 @@ WHERE gender = 'f'
 	WHERE to_date > CURDATE()
 );
 
-# 5. Find all the employees that currently have a higher than average salary.
-# 154543 rows in total. 
+-- 5. Find all the employees that currently have a higher than average salary.
+-- 154543 rows in total. 
 
 SELECT e.first_name, e.last_name, s.salary
 FROM (
@@ -66,7 +66,7 @@ JOIN salaries as s
 WHERE s.to_date > CURDATE()
 AND s.salary > avg_s.avg_sal;
 
-# 6. How many current salaries are within 1 standard deviation of the highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this? 78 salaries
+-- 6. How many current salaries are within 1 standard deviation of the highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this? 78 salaries
 
 SELECT COUNT(s.salary)
 FROM (
@@ -85,3 +85,49 @@ JOIN salaries as s
 WHERE s.to_date > CURDATE()
 AND max_s.max_sal - sd_s.sd_sal <= s.salary;
 
+-- BONUS
+-- 1. Find all the department names that currently have female managers
+SELECT dept_name
+FROM departments as d
+JOIN dept_emp as de
+  ON de.dept_no = d.dept_no
+JOIN employees as e
+  ON e.emp_no = de.emp_no
+WHERE e.emp_no IN (
+	SELECT emp_no
+	FROM employees
+	WHERE gender = 'f'
+  	AND emp_no IN (
+		SELECT emp_no
+		FROM dept_manager
+		WHERE to_date > CURDATE()
+));
+
+-- 2. Find the first and last name of the employee with the highest salary.
+
+SELECT first_name, last_name
+FROM employees AS e
+JOIN salaries AS s
+  ON s.emp_no = e.emp_no
+JOIN (
+	SELECT MAX(salary) as max_column
+	FROM salaries
+) AS max_table
+WHERE max_table.max_column = s.salary
+;
+
+-- 3. Find the department name that the employee with the highest salary works in
+
+SELECT d.dept_name
+FROM employees AS e
+JOIN salaries AS s
+  ON s.emp_no = e.emp_no
+JOIN (
+	SELECT MAX(salary) as max_column
+	FROM salaries
+) AS max_table
+JOIN dept_emp as de
+  ON de.emp_no = e.emp_no
+JOIN departments as d
+  ON d.dept_no = de.dept_no
+WHERE max_table.max_column = s.salary;
